a7588b1443e676f33f65d993a70e9ff728ca129c
KingShakeJS <kingshakejs@yandex.ru>
2024-01-22 13:26:30 +0300

    com3

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-function evenOrOdd(number) {
-  if (number % 2 ===0){
-      return "Even";
-  }
-  else{
-      return "Odd";
-  }
+// function evenOrOdd(number) {
+//     if (number % 2 === 0) {
+//         return "Even";
+//     } else {
+//         return "Odd";
+//     }
+// }
+//
+// function makeNegative(num) {
+//     if (num > 0) {
+//         return -num
+//     } else if (num < 0) {
+//         return num
+//     } else {
+//         return 0
+//     }
+// }
+
+function basicOp(operation, value1, value2){
+if(operation==="-"){
+    return value1 - value2;
 }
+else if (operation==="+"){
+    return value1 + value2;
+}
+else if (operation==="/"){
+    return value1 / value2;
+}
+else if (operation==="*"){
+    return value1 * value2;
+}
+
+
+}
+
 
