764f587eb7e60ac2b8e1f47581d61606ec0bd1ec
KingShakeJS <kingshakejs@yandex.ru>
2024-01-23 12:55:29 +0300

    com7

--- index.js
+++ index.js
@@ -72,3 +72,10 @@
 //     }
 //     return str;
 // }
+
+
+// function getSize(width, height, depth){
+//     let volume = width * height * depth;
+//     let area = 2*(width * height + height * depth + width * depth);
+//     return[area, volume]
+// }
