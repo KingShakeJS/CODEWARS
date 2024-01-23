917c8b2efa5241b46b747e881ca244c1caac9c04
KingShakeJS <kingshakejs@yandex.ru>
2024-01-23 13:05:28 +0300

    com8

--- index.js
+++ index.js
@@ -79,3 +79,13 @@
 //     let area = 2*(width * height + height * depth + width * depth);
 //     return[area, volume]
 // }
+
+
+
+// function getAverage(marks){
+//     let sum=0;
+//     for (let i =0; i<marks.length; i++){
+//         sum = sum + marks[i]
+//     }
+//     return Math.floor(sum / marks.length)
+// }
