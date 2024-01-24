f50093007770adcc61628589e8164a9108e58160
KingShakeJS <kingshakejs@yandex.ru>
2024-01-24 19:26:23 +0300

    com22

--- index.js
+++ index.js
@@ -271,3 +271,11 @@
 //          return 'I smell a series!'
 //      }
 // }
+
+// var findAverage = function (nums) {
+//     let sum=0;
+//     for (let i=0; i<nums.length; i++){
+//         sum=sum+nums[i]
+//     }
+//     return sum / nums.length
+// }
